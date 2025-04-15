8c304be84eb4484e32a12f475a745f9fd6be81bd
YaroShkvorets <shkvorets@gmail.com>
2025-04-15 00:50:53 -0400

    Add head block metrics

--- sinker/metrics.go
+++ sinker/metrics.go
@@ -13,3 +13,6 @@ var metrics = dmetrics.NewSet()
 var FlushCount = metrics.NewCounter("substreams_sink_postgres_store_flush_count", "The amount of flush that happened so far")
 var FlushedRowsCount = metrics.NewCounter("substreams_sink_postgres_flushed_rows_count", "The number of flushed rows so far")
 var FlushDuration = metrics.NewCounter("substreams_sink_postgres_store_flush_duration", "The amount of time spent flushing cache to db (in nanoseconds)")
+
+var HeadBlockNumber = metrics.NewHeadBlockNumber("substreams_sink_postgres")
+var HeadBlockTimeDrift = metrics.NewHeadTimeDrift("substreams_sink_postgres")
